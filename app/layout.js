cc860bc6bbc66d3207d4eeb2225e263ea6a44cb8
finnius25 <finneykoshy@gmail.com>
2023-10-13 14:45:05 -0500

    update analytics

--- app/layout.js
+++ app/layout.js
@@ -48,14 +48,14 @@ export default function RootLayout({ children }) {
         <meta property="og:type" content="website"></meta>
         <Script
           async
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          src={`https://www.googletagmanager.com/gtag/js?id=G-9X4NEJ7Z5T`}
         ></Script>
         <Script id="google-analytics">
           {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
 
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}'); `}
+        gtag('config', 'G-9X4NEJ7Z5T'); `}
         </Script>
       </head>
 
