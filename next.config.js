8efe32a0ea78396ad3fd4237d5aa669bcbc6077b
Ajoy Thomas <ajoythomas@gmail.com>
2024-12-16 11:37:30 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/sets/:setId',
+        destination: 'worshipbuddy://sets?:setId',
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
