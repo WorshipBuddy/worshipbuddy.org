f98c1fad4361c640b461cdc0f24f77058c41986f
Ajoy Thomas <ajoythomas@gmail.com>
2024-12-16 11:56:39 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/sets/:setId',
-        destination: 'worshipbuddy://sets?:setId',
+        destination: 'worshipbuddy://sets?setId=:setId',
         permanent: true,
       },
     ];
