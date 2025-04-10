dec5b184e936c89e3362742fc63189db957ca89a
JoelRoy5 <roycjoel@gmail.com>
2025-04-10 12:10:15 -0500

    fixed sizing on Mission page, text no longer moves off top of screen when resizing

--- app/mission/page.jsx
+++ app/mission/page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function page() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center text-white bg-neutral-900 px-4">
+    <div className="min-h-screen flex flex-col items-center justify-center text-white bg-neutral-900 px-4 py-3">
       <h1 className="font-semibold text-4xl md:text-5xl ">Mission</h1>
       <div className="h-0.5 w-full md:w-8/12 bg-neutral-50 my-4"></div>
       <div className="flex flex-col gap-20 items-center justify-center">
